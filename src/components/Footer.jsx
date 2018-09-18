2b0e55d4ec1522ff3e57d395b7662e9519d5cc71
Cody Ogden <cody@codyogden.com>
2018-09-18 01:03:03 -0500

    Resolve #19. Change Encryped Search URL.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,45 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 // Import Styled Components
 import { FooterContainer } from './Footer.atoms';
 
-export default class Footer extends Component {
-  componentWillMount() {
-    this.setState({
-      links: [
-        {
-          href: 'https://github.com/codyogden/killedbygoogle',
-          text: 'GitHub',
-        },
-        {
-          href: 'https://codyogden.com',
-          text: 'Cody Ogden',
-        },
-      ],
-    });
-  }
-
-  footerMenu() {
-    const { links } = this.state;
-    // Array to attach components
-    const componentLinksArry = [];
-    // Map each link
-    links.map((link, index) => {
-      componentLinksArry.push();
-      // Only add a dash if the link is not the final one in the array
-      if (index !== (links.length - 1)) {
-        return componentLinksArry.push(`${<a href={link.href} key={link.href}>{link.text}</a>} - `);
-      }
-      return componentLinksArry.push(`${<a href={link.href} key={link.href}>{link.text}</a>}`);
-    });
-    return componentLinksArry;
-  }
-
-  render() {
-    return (
-      <FooterContainer>
-        {this.footerMenu()}
-      </FooterContainer>
-    );
-  }
-}
+export default () => (
+  <FooterContainer>
+    <a href="https://github.com/codyogden/killedbygoogle">GitHub</a>
+    {' - '}
+    <a href="https://codyogden.com">Cody Ogden</a>
+  </FooterContainer>
+);
