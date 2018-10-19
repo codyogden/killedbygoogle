8a3c7ef54bccff042437b7de1f2778a4b0245115
Cody Ogden <cody@codyogden.com>
2018-10-18 22:05:15 -0500

    Update graveyard date test.

--- graveyard.test.js
+++ graveyard.test.js
@@ -1,12 +1,12 @@
-const { error } = require('console');
 const data = require('./graveyard.json');
 
-try {
-  data.forEach((product) => {
+data.forEach((product) => {
+  try {
     if ((new Date(product.dateClose) - new Date(product.dateOpen)) < 0) {
-      throw Object({ error: 'Product dates are incorrect.', product });
+      throw (new Error('Product dates are incorrect.', product));
     }
-  });
-} catch (err) {
-  error(err);
-}
+  } catch (err) {
+    console.log(product);
+    throw err;
+  }
+});
