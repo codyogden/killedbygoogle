2b34872c0bee2cf0b821cd66cf9af48ae213da51
Ngoc Tung Ngo <t.ngo@micromata.de>
2018-10-19 16:46:12 +0200

    migrate graveyard.test.js

--- graveyard.test.js
+++ graveyard.test.js
@@ -3,8 +3,14 @@ const data = require('./graveyard.json');
 
 describe('graveyard', () => {
     it('graveyard', () => {
-        expect(data).toHaveLength(82);
+        expect(data).toHaveLength(84);
         data.forEach((product) => {
+            expect(product.dateClose).not.toBeNull();
+            expect(product.dateOpen).not.toBeNull();
+            expect(product.description).not.toBeNull();
+            expect(product.link).not.toBeNull();
+            expect(product.name).not.toBeNull();
+
             let dateClose = new Date(product.dateClose).getTime();
             let dateOpen = new Date(product.dateOpen).getTime();
             expect(dateOpen).toBeLessThanOrEqual(dateClose);
