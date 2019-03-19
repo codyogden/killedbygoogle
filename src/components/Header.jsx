4834570ce566e633a4728730c8b469efe5733c58
Cody Ogden <cody@codyogden.com>
2019-03-19 12:55:00 -0500

    Complete ready for Netlify.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Tombstone from '../assets/tombstone.svg';
+
 // Import Styled Components
 import { Logo, Masthead, Title } from './Header.atoms';
 
 export default () => (
   <Masthead>
-    <Logo src="assets/tombstone.svg" alt="Tombstone" />
+    <Logo src={Tombstone} alt="Tombstone" />
     <Title>
       {'Killed by Google'}
     </Title>
