f7cf5432a37a17f81c91dcf1fcf45a08729040b6
Cody Ogden <cody@codyogden.com>
2019-04-05 17:05:22 -0500

    Resovle linting errors.

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { format, formatDistance, parseISO } from 'date-fns';
+import { format } from 'date-fns';
 
 import Knife from '../assets/knife.svg';
 
