27e5a9408b209fd757aca17c7c6a0348bd321fa7
easrng <easrng@gmail.com>
2020-12-30 23:44:19 -0500

    Update template.js

--- story/template.js
+++ story/template.js
@@ -21,259 +21,339 @@ module.exports=graveyard=>`<!doctype html>
     <meta property="twitter:title" content="The Chopping Block - Killed by Google" />
     <meta name="twitter:card" content="summary" />
     <link rel="canonical" href="https://killedbygoogle.com/story/">
-    <style amp-custom>
-        .whitebg{
-              background : rgb(255,255,255);
-           }
-           .main-guillotine-wrap{
-              height : 42.67%;
-              left : 14.17%;
-              top : 26.33%;
-              width : 71.11%;
-           }
-           .chopping-block-title{
-              color : rgb(51, 51, 51);
-              font-family : Roboto;
-              font-size : 3.11em;
-              font-weight : 300;
-              left : 4.72%;
-              letter-spacing : 0.03em;
-              line-height : 1.4em;
-              top : 11.17%;
-              width : 90.56%;
-           }
-           .kbg-name{
-              color : rgb(51, 51, 51);
-              font-family : Roboto;
-              font-size : 1.42em;
-              font-weight : 400;
-              left : 15.28%;
-              letter-spacing : 0.06em;
-              line-height : 1.25em;
-              text-align : center;
-              top : 94.33%;
-              width : 69.44%;
-           }
-           .kbg-logo-wrap#logo img{
-              object-fit : cover;
-              object-position : 0% 0%;
-              max-width : unset;
-              max-height : unset;
-              margin : unset;
-              top : 0.00%;
-              height : 100.00%;
-              left : 0.00%;
-              width : 100.00%;
-           }
-           .kbg-logo-wrap amp-img img{
-              object-fit : cover;
-              object-position : 0% 0%;
-           }
-           .kbg-logo-wrap amp-img{
-              transform : translateX(-50%) translateY(-50%) ;
-           }
-           .kbg-logo-wrap{
-              height : 4.00em;
-              left : 49.86%;
-              top : 89.42%;
-              width : 4.00em;
-           }
-           .grave-guillotine-wrap#guillotine img{
-              object-fit : cover;
-              object-position : 0% 0%;
-              max-width : unset;
-              max-height : unset;
-              margin : unset;
-              top : 0.00%;
-              height : 100.00%;
-              left : 0.00%;
-              width : 100.00%;
-           }
-           .grave-guillotine-wrap amp-img img{
-              object-fit : cover;
-              object-position : 0% 0%;
-           }
-           .grave-guillotine-wrap amp-img{
-              transform : translateX(-50%) translateY(-50%) ;
-           }
-           .grave-guillotine-wrap{
-              height : 13.33em;
-              left : 49.45%;
-              top : 18.33%;
-              width : 13.33em;
-           }
-           .grave-name{
-              color : rgb(51, 51, 51);
-              font-family : Roboto;
-              font-size : 3.11em;
-              font-weight : 300;
-              left : 15.00%;
-              letter-spacing : 0.03em;
-              line-height : 1.4em;
-              text-align : center;
-              top : 41.83%;
-              width : 69.44%;
-           }
-           .grave-desc{
-              color : rgb(51, 51, 51);
-              font-family : Roboto;
-              font-size : 1.42em;
-              font-weight : 400;
-              left : 14.72%;
-              letter-spacing : 0.06em;
-              line-height : 1.4em;
-              text-align : center;
-              top : 52.83%;
-              width : 69.44%;
-           }
-           .grave-death{
-              color : rgb(51, 51, 51);
-              font-family : Roboto;
-              font-size : 1.24em;
-              font-weight : 500;
-              left : 26.94%;
-              letter-spacing : 0.07em;
-              line-height : 1.43em;
-              text-align : center;
-              top : 34.50%;
-              width : 44.72%;
-           }
-           .read-more{
-              background : rgba(0,0,0,1);
-              border-color : rgb(0, 0, 0);
-              border-radius : 100px 100px 100px 100px;
-              border-style : solid;
-              border-width : 0px 0px 0px 0px;
-              color : rgba(255,255,255,1);
-              font-family : Roboto;
-              font-size : 1.42em;
-              font-weight : 500;
-              left : 25.00%;
-              letter-spacing : 0.06em;
-              line-height : 1.25em;
-              padding : 9px 9px 9px 9px;
-              text-align : center;
-              top : 90.00%;
-              width : 48.33%;
-           }
-           amp-img{ position: relative }
-           .content-block{ padding: 10px 15px }
-           .block{ position: relative; padding: 5px;word-break: break-word;}
-           .content-block amp-video{ display: block; margin: 0 auto}
-           .content-block > hr {padding: 0;margin: 5px 0;border-top: 1px solid #eee}
-           .block a:not(.cta-a) {color: inherit;text-decoration: none}
-           p, h1, h2, h3, h4, h5, h6{ padding: 0;margin: 0 }
-           .cta-a{
-              text-decoration: none;
-              display: flex; 
-              align-items: center; 
-           }
-           .cta-a span{ 
-              width:100%; 
-           }
-           .svg-el svg{ 
-              display: block;
-              width: 100%;
-              height: 100%;
-            
-           }
-           .block .cta-a{ 
-              padding: 0.3em 1em;
-              display: inline-block;
-           }
-           *{
-              -webkit-box-sizing: border-box;
-              -moz-box-sizing: border-box;
-              box-sizing: border-box;
-           }
-           .cta-a amp-img{
-              width: 100%;
-              height: 100%;
-           }
-           .cta-a amp-img img{ 
-              height:100%;
-              width: 100%;
-              min-height: unset;
-           }
-           .flip-vertically img, .flip-vertically svg{
-              transform: rotateY(180deg);
-           }
-           .flip-horizontally img, .flip-horizontally svg{
-              transform: rotateX(180deg);
-           }
-           .flip-horizontally.flip-vertically img, .flip-horizontally.flip-vertically svg{
-              transform: rotateX(180deg) rotateY(180deg);
-           }
-           .pa{
-              position: absolute;
-              word-break: break-word;
-           }
-           .txt-hl{
-              -webkit-box-decoration-break: clone;
-              -o-box-decoration-break: clone;
-              box-decoration-break: clone;
-           }
-           .img-fill,a.story-tooltip{
-              top:0;
-              left:0;
-              width:100%;
-              height:100%
-           }
-           a.story-tooltip{
-        
-              text-decoration: none;
-              color: inherit;
-           }
-           .offset{
-              transform: translateX(-50%) translateY(-50%)
-           }
-           .oh{
-              overflow: hidden;
-           }
-           .pa.kbimg{ 
-              width: 0; 
-              height: 0 
-           }
-           .pa-list-type{
-              padding:5px;
-              padding-left:19px;
-              margin:5px 0 0 0;
-           }
-           ol.pa-list-type{
-              list-style:lower-alpha;
-           }
-           .block .pa-list-type li a{color: #1890ff;}
-           
-           amp-story{
-              font-size:3.125vw
-           }
-           @media screen and (min-aspect-ratio: 3/5) and (max-aspect-ratio: 5/5){
-           amp-story {
-              font-size: 1.875vh
-           }
-           .letterbox{
-              width:60vh;
-              height: 100vh;
-              position: absolute;
-              top: 50%;
-              left: 50%;
-              transform: translate(-50%, -50%);
-           }
-           amp-story-cta-layer .letterbox{
-              height: 20vh;
-           }
-           }
-           @media screen and (min-width:1024px){
-              amp-story{font-size:1.8vh}
-           }
-           @media screen and (min-width:1024px) and (max-height:660px){
-              amp-story{font-size:1.8vh}
-           }
-           ::cue {
-              background-color: rgba(0, 0, 0, 0.75);
-              font-size: 24px;
-              line-height: 1.5;
-           }
+    <style amp-custom>.whitebg {
+    background : rgb(255,255,255);
+}
+
+.main-guillotine-wrap {
+    height : 42.67%;
+    left : 14.17%;
+    top : 26.33%;
+    width : 71.11%;
+}
+
+.chopping-block-title {
+    color : rgb(51, 51, 51);
+    font-family : Roboto;
+    font-size : 3.11em;
+    font-weight : 300;
+    left : 4.72%;
+    letter-spacing : 0.03em;
+    line-height : 1.4em;
+    top : 11.17%;
+    width : 90.56%;
+}
+
+.kbg-name {
+    color : rgb(51, 51, 51);
+    font-family : Roboto;
+    font-size : 1.42em;
+    font-weight : 400;
+    left : 15.28%;
+    letter-spacing : 0.06em;
+    line-height : 1.25em;
+    text-align : center;
+    top : 94.33%;
+    width : 69.44%;
+}
+
+.kbg-logo-wrap#logo img {
+    object-fit : cover;
+    object-position : 0% 0%;
+    max-width : unset;
+    max-height : unset;
+    margin : unset;
+    top : 0.00%;
+    height : 100.00%;
+    left : 0.00%;
+    width : 100.00%;
+}
+
+.kbg-logo-wrap amp-img img {
+    object-fit : cover;
+    object-position : 0% 0%;
+}
+
+.kbg-logo-wrap amp-img {
+    transform : translateX(-50%) translateY(-50%) ;
+}
+
+.kbg-logo-wrap {
+    height : 4.00em;
+    left : 49.86%;
+    top : 89.42%;
+    width : 4.00em;
+}
+
+.grave-guillotine-wrap#guillotine img {
+    object-fit : cover;
+    object-position : 0% 0%;
+    max-width : unset;
+    max-height : unset;
+    margin : unset;
+    top : 0.00%;
+    height : 100.00%;
+    left : 0.00%;
+    width : 100.00%;
+}
+
+.grave-guillotine-wrap amp-img img {
+    object-fit : cover;
+    object-position : 0% 0%;
+}
+
+.grave-guillotine-wrap amp-img {
+}
+
+.grave-guillotine-wrap {
+    height : 13.33em;
+    width : 13.33em;
+    order: 0;
+}
+
+.grave-name {
+    color : rgb(51, 51, 51);
+    font-family : Roboto;
+    font-size : 3.11em;
+    font-weight : 300;
+    letter-spacing : 0.03em;
+    line-height : 1.4em;
+    text-align : center;
+    order: 2;
+    margin-top: 2rem;
+}
+
+.grave-desc {
+    color : rgb(51, 51, 51);
+    font-family : Roboto;
+    font-size : 1.42em;
+    font-weight : 400;
+    letter-spacing : 0.06em;
+    line-height : 1.4em;
+    text-align : center;
+    order: 3;
+    flex-grow: 1;
+    margin: 0.5rem 0;
+}
+
+.grave-death {
+    color : rgb(51, 51, 51);
+    font-family : Roboto;
+    font-size : 1.24em;
+    font-weight : 500;
+    letter-spacing : 0.07em;
+    line-height : 1.43em;
+    text-align : center;
+    order: 1;
+    margin-top: 1rem;
+}
+
+.read-more {
+    background : rgba(0,0,0,1);
+    border-color : rgb(0, 0, 0);
+    border-radius : 100px 100px 100px 100px;
+    border-style : solid;
+    border-width : 0px 0px 0px 0px;
+    color : rgba(255,255,255,1);
+    font-family : Roboto;
+    font-size : 1.42em;
+    font-weight : 500;
+    letter-spacing : 0.06em;
+    line-height : 1.25em;
+    padding : 9px 9px 9px 9px;
+    text-align : center;
+    width: 8em;
+    order: 999;
+}
+
+amp-img {
+    position: relative
+}
+
+.content-block {
+    padding: 10px 15px
+}
+
+.block {
+    position: relative;
+    padding: 5px;
+    word-break: break-word;
+}
+
+.content-block amp-video {
+    display: block;
+    margin: 0 auto
+}
+
+.content-block > hr {
+    padding: 0;
+    margin: 5px 0;
+    border-top: 1px solid #eee
+}
+
+.block a:not(.cta-a) {
+    color: inherit;
+    text-decoration: none
+}
+
+p, h1, h2, h3, h4, h5, h6 {
+    padding: 0;
+    margin: 0
+}
+
+.cta-a {
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+}
+
+.cta-a span {
+    width: 100%;
+}
+
+.svg-el svg {
+    display: block;
+    width: 100%;
+    height: 100%;
+}
+
+.block .cta-a {
+    padding: 0.3em 1em;
+    display: inline-block;
+}
+
+* {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+}
+
+.cta-a amp-img {
+    width: 100%;
+    height: 100%;
+}
+
+.cta-a amp-img img {
+    height: 100%;
+    width: 100%;
+    min-height: unset;
+}
+
+.flip-vertically img, .flip-vertically svg {
+    transform: rotateY(180deg);
+}
+
+.flip-horizontally img, .flip-horizontally svg {
+    transform: rotateX(180deg);
+}
+
+.flip-horizontally.flip-vertically img, .flip-horizontally.flip-vertically svg {
+    transform: rotateX(180deg) rotateY(180deg);
+}
+
+.pa {
+    word-break: break-word;
+}
+
+.txt-hl {
+    -webkit-box-decoration-break: clone;
+    -o-box-decoration-break: clone;
+    box-decoration-break: clone;
+}
+
+.img-fill,a.story-tooltip {
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%
+}
+
+a.story-tooltip {
+    text-decoration: none;
+    color: inherit;
+}
+
+.offset {
+    transform: translateX(-50%) translateY(-50%)
+}
+
+.oh {
+    overflow: hidden;
+}
+
+.pa.kbimg {
+    width: 0;
+    height: 0
+}
+
+.pa-list-type {
+    padding: 5px;
+    padding-left: 19px;
+    margin: 5px 0 0 0;
+}
+
+ol.pa-list-type {
+    list-style: lower-alpha;
+}
+
+.block .pa-list-type li a {
+    color: #1890ff;
+}
+
+amp-story {
+    font-size: 3.125vw
+}
+
+@media screen and (min-aspect-ratio: 3/5) and (max-aspect-ratio: 5/5) {
+    amp-story {
+        font-size: 1.875vh
+    }
+
+    .letterbox {
+        width: 60vh;
+        height: 100vh;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    amp-story-cta-layer .letterbox {
+        height: 20vh;
+    }
+}
+
+@media screen and (min-width: 1024px) {
+    amp-story {
+        font-size:1.8vh
+    }
+}
+
+@media screen and (min-width: 1024px) and (max-height:660px) {
+    amp-story {
+        font-size:1.8vh
+    }
+}
+
+::cue {
+    background-color: rgba(0, 0, 0, 0.75);
+    font-size: 24px;
+    line-height: 1.5;
+}
+
+amp-story-page:not(.page1) amp-story-grid-layer .letterbox {
+    display: flex !important;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    width: 100%;
+    height: 100% !important;
+    flex-grow: 1;
+    position: absolute;
+    padding: 3rem 2rem;
+}
+
     </style>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?display=swap&family=Roboto:400,300,500" media="all">
 </head>
