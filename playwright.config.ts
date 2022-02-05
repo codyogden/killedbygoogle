800c672fb1bb935028f9bf160fbf77a330772ed5
Cody Ogden <cody@codyogden.com>
2022-02-05 01:44:16 -0600

    test(filter): pull graveyard count data dynamically to run against test

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,7 +40,7 @@ const config: PlaywrightTestConfig = {
     actionTimeout: 0,
 
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL,
+    baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL ?? 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
