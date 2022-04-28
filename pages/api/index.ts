d8c188d6daf45cc0feafad613ce7a10e6fffa774
Cody Ogden <cody@codyogden.com>
2022-04-27 19:00:04 -0500

    umami

--- pages/api/index.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
-
-const umamiID: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-    res.status(200).send(process.env.UMAMI_ID);
-}
-
-export default umamiID;
