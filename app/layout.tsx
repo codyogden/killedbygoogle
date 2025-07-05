f6693a6c83b5422c94640dae879a9506f0da21f5
Cody Ogden <cody@codyogden.com>
2025-07-04 20:01:49 -0700

    favicon work

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,21 @@
 // Global Stylesheet
 import './globals.css';
 import { ReactNode } from 'react';
+import { Metadata } from 'next';
 
 declare global {
     interface Window {
         umami?: any;
     }
 }
+
+export const metadata: Metadata = {
+    icons: {
+        icon: [
+            { url: '/favicon.png', type: 'image/png' },
+        ],
+    },
+};
 
 export default function RootLayout({
     children,
