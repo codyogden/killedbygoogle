eda58339027d83e267dcff674b5ce8efa685f015
Cody Ogden <cody@codyogden.com>
2025-07-04 19:54:54 -0700

    Update jest config.

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+/** @type {import('jest').Config} */
+const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testMatch: ['**/*.test.(ts|tsx|js|jsx)'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/.next/**',
+  ],
+};
+
+module.exports = config; 
