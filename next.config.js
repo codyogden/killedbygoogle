3cf3be00fa987160064246efe73937addc704f41
Cody Ogden <cody@codyogden.com>
2025-12-01 10:45:02 -0800

    Upgrade Next.js, deps, devDeps

--- next.config.js
+++ next.config.js
@@ -12,14 +12,6 @@ const nextConfig = {
             },
         },
     },
-    webpack(config) {
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: ["@svgr/webpack"]
-        });
-
-        return config;
-    },
     async redirects() {
         return [
             {
